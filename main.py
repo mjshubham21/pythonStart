9f7a9d6f41ab69fb7f97d66eaf7165669ad0f4fe
mjshubham21 <mjshubhampawar21@gmail.com>
2024-10-15 22:13:01 +0530

    first commit of Python Journey

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello World!!")
+print(21)
+
+# 15-10-2024/ TUESDAY.
+
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/mjshubham21/pythonStart.git
+# git push -u origin main
